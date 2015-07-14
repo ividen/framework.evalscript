c0e73b7059a8b295b6e426091393bbaf1c8922c9
aguzanov <alex.guzanov@gmail.com>
2015-07-14 16:42:03 +0300

    Basic arithmetic operations

--- src/main/scala/com/ividen/evalscript/Interpeter.scala
+++ src/main/scala/com/ividen/evalscript/Interpeter.scala
@@ -5,8 +5,8 @@ object Interpreter {
   private def processElement(e: ProgramElement) = e match {
     case exp: Expression => println(processExpression(exp))
   }
-  private def processExpression(e: Expression): BigDecimal = e match {
-    case LiteralExpression(l) => l.value.asInstanceOf[BigDecimal]
+  private def processExpression(e: Expression): Val = e match {
+    case LiteralExpression(l) => Val(l.value)
     case PlusExpression(l, r) => processExpression(l) + processExpression(r)
     case MinusExpression(l, r) => processExpression(l) - processExpression(r)
     case DivideExpression(l, r) => processExpression(l) / processExpression(r)
@@ -15,11 +15,55 @@ object Interpreter {
   }
 }
 
+case class Val(value: Any) {
 
-object Main2 extends EvalScriptParser{
+  def +(v: Val): Val = (value, v.value) match {
+    case (l: BigDecimal, r: BigDecimal) => Val(l + r)
+    case (l: BigDecimal, r: String) => Val(l + BigDecimal(r))
+    case (l: BigDecimal, r: Boolean) => Val(l + BigDecimal(if (r) 1 else 0))
+    case (l: String, r: String) => Val(l + r)
+    case (l: String, r: BigDecimal) => Val(l + r.toString)
+    case (l: String, r: Boolean) => Val(l + r.toString)
+    case x  => notSupported("+",x)
+  }
+
+  def -(v: Val): Val = (value, v.value) match {
+    case (l: BigDecimal, r: BigDecimal) => Val(l - r)
+    case (l: BigDecimal, r: String) => Val(l - BigDecimal(r))
+    case (l: BigDecimal, r: Boolean) => Val(l - BigDecimal(if (r) 1 else 0))
+    case x  => notSupported("-",x)
+  }
+
+  def *(v: Val): Val = (value, v.value) match {
+    case (l: BigDecimal, r: BigDecimal) => Val(l * r)
+    case (l: BigDecimal, r: String) => Val(l * BigDecimal(r))
+    case (l: BigDecimal, r: Boolean) => Val(l * BigDecimal(if (r) 1 else 0))
+    case x  => notSupported("*",x)
+  }
+
+  def /(v: Val): Val = (value, v.value) match {
+    case (l: BigDecimal, r: BigDecimal) => Val(l * r)
+    case (l: BigDecimal, r: String) => Val(l / BigDecimal(r))
+    case (l: BigDecimal, r: Boolean) => Val(l / BigDecimal(if (r) 1 else 0))
+    case x  => notSupported("/",x)
+  }
+
+  def %(v: Val): Val = (value, v.value) match {
+    case (l: BigDecimal, r: BigDecimal) => Val(l * r)
+    case (l: BigDecimal, r: String) => Val(l % BigDecimal(r))
+    case (l: BigDecimal, r: Boolean) => Val(l % BigDecimal(if (r) 1 else 0))
+    case x  => notSupported("%",x)
+  }
+
+  private def notSupported(o: String, x: (Any, Any)): Val  = throw new UnsupportedOperationException(s"Can't do  ${x._1}:${x._1.getClass.getName} ${o} ${x._2}:${x._2.getClass.getName}")
+}
+
+object Main2 extends EvalScriptParser {
   def main(args: Array[String]) {
-     val s = "1+1+1+1-2-2-2"
+    val s = """ 'Test "1"' + " and " +"Test '2'" + (10 *10)  """
 
-     Interpreter.process(parseAll(program,s).get)
+    val res = parseAll(program, s).get
+    println(res)
+    Interpreter.process(res)
   }
 }
