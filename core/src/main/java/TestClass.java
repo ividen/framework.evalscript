f31c01528eb13e040de979393d2c4dcc4c0a8094
aguzanov <alex.guzanov@gmail.com>
2015-07-24 10:05:02 +0300

    working on compilation

--- core/src/main/java/TestClass.java
+++ core/src/main/java/TestClass.java
@@ -1,16 +1,20 @@
-import com.ividen.evalscript.BooleanLiteral;
-import com.ividen.evalscript.DecimalLiteral;
-import com.ividen.evalscript.Literal;
-import com.ividen.evalscript.StringLiteral;
+import com.ividen.evalscript.*;
 import scala.math.BigDecimal;
 
 
 /**
  * Created by alexander.guzanov on 7/23/15.
  */
-public class TestClass {
+public class TestClass extends CompiledScript{
     static final Literal v1 = new DecimalLiteral(BigDecimal.valueOf(10));
-    static final Literal v2 = new StringLiteral("test");
-    static final Literal v3 = new BooleanLiteral(true);
+    static final Literal v2 = new DecimalLiteral(BigDecimal.valueOf(20));
+
+    public TestClass(GlobalContext globals) {
+        super(globals);
+    }
+    @Override
+    public void execute() {
+      v1.$plus(v2);
+    }
 
 }
--- core/src/main/scala/com/ividen/evalscript/Compiler.scala
+++ core/src/main/scala/com/ividen/evalscript/Compiler.scala
@@ -11,6 +11,7 @@ import org.objectweb.asm.Opcodes._
 import scala.annotation.tailrec
 import scala.collection
 import scala.collection.parallel.mutable
+import scala.reflect.ClassTag
 
 
 abstract class CompiledScript(val globals: GlobalContext) {
@@ -18,12 +19,11 @@ abstract class CompiledScript(val globals: GlobalContext) {
 }
 
 
-
 class DerivedCompiledScript(g: GlobalContext) extends CompiledScript(g) {
 
-  override def execute: Unit ={
+  override def execute: Unit = {
     val literal1 = DecimalLiteral(1)
-    val literal2  = DecimalLiteral(1)
+    val literal2 = DecimalLiteral(1)
 
     val literal3 = literal1 + literal2
     println(literal3)
@@ -31,142 +31,158 @@ class DerivedCompiledScript(g: GlobalContext) extends CompiledScript(g) {
 }
 
 
-object ScriptCompiler {
+private class Generator(b: `{}`, name: String) extends ClassLoader {
+  private val cn = new ClassNode(ASM4)
+  private val exec = new MethodNode(ACC_PUBLIC, "execute", "()V", null, Array.empty)
+  private val methods: java.util.List[MethodNode] = cn.methods.asInstanceOf[java.util.List[MethodNode]]
+  private val fields: java.util.List[FieldNode] = cn.fields.asInstanceOf[java.util.List[FieldNode]]
+  private var fieldNumber: Int = 0
+  private val literals: collection.mutable.Map[Literal, String] = new collection.mutable.HashMap[Literal, String]()
 
-  private class Generator(b: `{}`, name: String) extends ClassLoader{
-    val cn = new ClassNode(ASM4)
-    val exec = new MethodNode(ACC_PUBLIC, "execute", "()V", null, Array.empty)
-    val methods: java.util.List[MethodNode] = cn.methods.asInstanceOf[java.util.List[MethodNode]]
-    val fields: java.util.List[FieldNode] = cn.fields.asInstanceOf[java.util.List[FieldNode]]
-    var fieldNumber:Int = 0
-    val literals: collection.mutable.Map[Literal, String] = new collection.mutable.HashMap[Literal,String]()
+  def compile: Class[CompiledScript] = {
+    initClassName
+    generateConstructor
+    methods.add(exec)
+    b.items.foreach(processElement)
+    addIns(new InsnNode(RETURN))
+    initStaticVars
 
-    def compile : Class[CompiledScript] = {
-      initClassName
-      generateConstructor
-      methods.add(exec)
-      exec.instructions.add(new InsnNode(RETURN))
-      b.items.foreach(processElement)
-      initStaticVars
+    val cw = new ClassWriter(ClassWriter.COMPUTE_MAXS)
+    cn.accept(cw)
+    val result = cw.toByteArray
+    val file = new File("/Users/alexander.guzanov/prj/Test.class")
+    file.createNewFile()
+    val stream = new FileOutputStream(file, false)
+    stream.write(result)
+    stream.close()
 
-      val cw = new ClassWriter(ClassWriter.COMPUTE_MAXS)
-      cn.accept(cw)
-      val result =cw.toByteArray
-      val file = new File("/Users/alexander.guzanov/prj/Test.class")
-      file.createNewFile()
-      val stream = new FileOutputStream(file, false)
-      stream.write(result)
-      stream.close()
+    defineClass(name, result, 0, result.length).asInstanceOf[Class[CompiledScript]]
+  }
 
-      defineClass(name,result,0,result.length).asInstanceOf[Class[CompiledScript]]
-    }
-
-    def initStaticVars = {
-      if(!literals.isEmpty){
-        val m = new MethodNode(ACC_STATIC,"{}", "()V",null,Array.empty)
-        for(e <- literals){
-          e._1 match {
-            case DecimalLiteral(v) =>
-            case StringLiteral(v) =>
-            case BooleanLiteral(v) =>
-          }
+  private def initStaticVars = {
+    if (!literals.isEmpty) {
+      val m = new MethodNode(ACC_STATIC, "<clinit>", "()V", null, Array.empty)
+      for (e <- literals) {
+        e._1 match {
+          case DecimalLiteral(v) => initDecimal(m, e._2, v)
+          case StringLiteral(v) => initString(m, e._2, v)
+          case BooleanLiteral(v) => initBoolean(m, e._2, v)
         }
       }
+      m.instructions.add(new InsnNode(RETURN))
+      methods.add(m)
     }
-
-    private def initClassName: Unit = {
-      cn.version = V1_6
-      cn.access = ACC_PUBLIC
-      cn.name = name
-      cn.superName = "com/ividen/evalscript/CompiledScript"
-    }
-
-    private def generateConstructor: Unit = {
-      val constructor = new MethodNode(ACC_PUBLIC, "<init>", "(Lcom/ividen/evalscript/GlobalContext;)V", null, Array.empty)
-      constructor.visitVarInsn(ALOAD, 0)
-      constructor.visitVarInsn(ALOAD, 1)
-      constructor.visitMethodInsn(INVOKESPECIAL, "com/ividen/evalscript/CompiledScript", "<init>", "(Lcom/ividen/evalscript/GlobalContext;)V")
-      constructor.visitInsn(RETURN)
-      constructor.visitMaxs(1, 1)
-      constructor.visitEnd()
-      methods.add(constructor)
-    }
-
-    private def processElement(e: ScriptElement): Unit = e match {
-      case exp: Expression => processExpression(exp)
-      //    case DeclareVars(l) => l.foreach(declareVar)
-      //    case assignment: `=` => processAssignment(assignment)
-      //    case `if else`(i, e) => processIfElse(i, e)
-      //    case `while do`(e, b,p) => processWhileDo(e, b,p)
-      //    case `do while`(e, b) => processDoWhile(e, b)
-      //    case `switch`(e, c, d) => processSwitch(e, c, d)
-      //    case _: `break` => this.break
-      //    case _: `continue` => this.breakContinue
-      //    case b: `{}` => processNewBlock(b)
-    }
-
-    private def createField(l: Literal) = {
-      fieldNumber += 1
-      val n = s"v$fieldNumber"
-      val node = new FieldNode(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, s"v$fieldNumber", "Lcom/ividen/evalscript/Literal;", null, null)
-      fields.add(node)
-      literals += l -> node.name
-      node.name
-    }
-
-    private def processExpression(e: Expression):Unit = e match {
-      case LiteralExpression(l: Literal) => {
-        var name = literals.getOrElse(l, createField(l))
-
-      }
-      case `:+`(l, r) => {
-        processExpression(l)
-        processExpression(r)
-        //invoke special
-      }
-      //    case `:-`(l, r) => processExpression(l) - processExpression(r)
-      //    case `/`(l, r) => processExpression(l) / processExpression(r)
-      //    case `*`(l, r) => processExpression(l) * processExpression(r)
-      //    case `%`(l, r) => processExpression(l) % processExpression(r)
-      //    case `!:`(r) => !processExpression(r)
-      //    case `[]`(l,r) => processExpression(l).apply(processExpression(r))
-      //    case `~:`(r) => ~processExpression(r)
-      //    case `-:`(r) => -processExpression(r)
-      //    case `+:`(r) => +processExpression(r)
-      //    case `++:`(v) => val result = processExpression(GerVar(v)) + DecimalLiteral(BigDecimal(1)); processAssignment(`=`(v, LiteralExpression(result))); result
-      //    case `--:`(v) => val result = processExpression(GerVar(v)) + DecimalLiteral(BigDecimal(1)); processAssignment(`=`(v, LiteralExpression(result))); result
-      //    case `:++`(v) => val result = processExpression(GerVar(v)); processAssignment(`=`(v, LiteralExpression(result + DecimalLiteral(BigDecimal(1))))); result
-      //    case `:--`(v) => val result = processExpression(GerVar(v)); processAssignment(`=`(v, LiteralExpression(result - DecimalLiteral(BigDecimal(1))))); result
-      //    case `>>`(l, r) => processExpression(l) >> processExpression(r)
-      //    case `<<`(l, r) => processExpression(l) << processExpression(r)
-      //    case `&`(l, r) => processExpression(l) & processExpression(r)
-      //    case `^`(l, r) => processExpression(l) ^ processExpression(r)
-      //    case `|`(l, r) => processExpression(l) | processExpression(r)
-      //    case `&&`(l, r) => processExpression(l) && processExpression(r)
-      //    case `||`(l, r) => processExpression(l) || processExpression(r)
-      //    case `:==`(l, r) => processExpression(l) == processExpression(r)
-      //    case `:!=`(l, r) => processExpression(l) != processExpression(r)
-      //    case `<`(l, r) => processExpression(l) < processExpression(r)
-      //    case `>`(l, r) => processExpression(l) > processExpression(r)
-      //    case `>=`(l, r) => processExpression(l) >= processExpression(r)
-      //    case `<=`(l, r) => processExpression(l) <= processExpression(r)
-      //    case GerVar(v: LocalVariable) => localContext(v)
-      //    case GerVar(v: GlobalVairable) => globalContext(v)
-      //    case `call`(n,a) => FunctionInvoker.invoke(n,a.map(x => processExpression(mv,x)))
-    }
-
   }
-  def compile(s: Script) = new Generator(s.block,generateClassName(s)).compile
 
-  private def genConstructor(cw: ClassWriter) = {
-    val mv = cw.visitMethod(ACC_PUBLIC, "<init>", "(Lcom/ividen/evalscript/GlobalContext;)V", null, null)
-    mv.visitVarInsn(ALOAD, 0)
-    mv.visitVarInsn(ALOAD, 1)
-    mv.visitMethodInsn(INVOKESPECIAL, "com/ividen/evalscript/CompiledScript", "<init>", "(Lcom/ividen/evalscript/GlobalContext;)V")
-    mv.visitInsn(RETURN)
-    mv.visitMaxs(1, 1)
-    mv.visitEnd()
+  private def addIns(il: AbstractInsnNode*) = il.foreach(exec.instructions.add)
+  private def addIns(m: MethodNode, il: AbstractInsnNode*) = il.foreach(m.instructions.add)
+
+  private def initDecimal(m: MethodNode, n: String, v: BigDecimal): Unit = addIns(m, new TypeInsnNode(NEW, typeString[DecimalLiteral]), new InsnNode(DUP)
+    , new LdcInsnNode(v.doubleValue()), new MethodInsnNode(INVOKESTATIC, typeString[BigDecimal], "valueOf", s"(D)${typeSignature[BigDecimal]}")
+    , new MethodInsnNode(INVOKESPECIAL, typeString[DecimalLiteral], "<init>", s"(${typeSignature[BigDecimal]})V"), new FieldInsnNode(PUTSTATIC, cn.name, n, typeSignature[Literal]))
+
+  private def initString(m: MethodNode, n: String, v: String): Unit = addIns(m, new TypeInsnNode(NEW, typeString[StringLiteral]), new InsnNode(DUP)
+    , new LdcInsnNode(v), new MethodInsnNode(INVOKESPECIAL, typeString[StringLiteral], "<init>", s"(${typeSignature[String]})V")
+    , new FieldInsnNode(PUTSTATIC, cn.name, n, typeSignature[Literal]))
+
+  private def initBoolean(m: MethodNode, n: String, v: Boolean) = addIns(m, new TypeInsnNode(NEW, typeString[BooleanLiteral])
+    , new InsnNode(DUP)
+    , new LdcInsnNode(v)
+    , new MethodInsnNode(INVOKESPECIAL, typeString[BooleanLiteral], "<init>", s"(Z)V")
+    , new FieldInsnNode(PUTSTATIC, cn.name, n, typeSignature[Literal]))
+
+  private def initClassName: Unit = {
+    cn.version = V1_6
+    cn.access = ACC_PUBLIC
+    cn.name = name
+    cn.superName = typeString[CompiledScript]
   }
+
+
+  private def typeSignature[T](implicit tag: ClassTag[T]) = "L" + typeString[T](tag) + ";"
+  private def typeString[T](implicit tag: ClassTag[T]) = tag.runtimeClass.getName.replaceAll("\\.", "/")
+
+  private def generateConstructor: Unit = {
+    val constructor = new MethodNode(ACC_PUBLIC, "<init>", s"(${typeSignature[GlobalContext]})V", null, Array.empty)
+    constructor.visitVarInsn(ALOAD, 0)
+    constructor.visitVarInsn(ALOAD, 1)
+    constructor.visitMethodInsn(INVOKESPECIAL, typeString[CompiledScript], "<init>", s"(${typeSignature[GlobalContext]})V")
+    constructor.visitInsn(RETURN)
+    constructor.visitMaxs(1, 1)
+    constructor.visitEnd()
+    methods.add(constructor)
+  }
+
+  private def processElement(e: ScriptElement): Unit = e match {
+    case exp: Expression => processExpression(exp)
+    //    case DeclareVars(l) => l.foreach(declareVar)
+    //    case assignment: `=` => processAssignment(assignment)
+    //    case `if else`(i, e) => processIfElse(i, e)
+    //    case `while do`(e, b,p) => processWhileDo(e, b,p)
+    //    case `do while`(e, b) => processDoWhile(e, b)
+    //    case `switch`(e, c, d) => processSwitch(e, c, d)
+    //    case _: `break` => this.break
+    //    case _: `continue` => this.breakContinue
+    //    case b: `{}` => processNewBlock(b)
+  }
+
+  private def createField(l: Literal) = {
+    fieldNumber += 1
+    val n = s"v$fieldNumber"
+    val node = new FieldNode(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, s"v$fieldNumber", typeSignature[Literal], null, null)
+    fields.add(node)
+    literals += l -> node.name
+    node.name
+  }
+
+  private def getFieldName(l: Literal) = literals.getOrElse(l, createField(l))
+
+  private def processExpression(e: Expression): Unit = e match {
+    case LiteralExpression(l: Literal) => addIns(new FieldInsnNode(GETSTATIC, cn.name, getFieldName(l), typeSignature[Literal]))
+    case `:+`(l, r) => {
+      processExpression(l)
+      processExpression(r)
+      addIns(new MethodInsnNode(INVOKEINTERFACE, typeString[Literal], "$plus", s"(${typeSignature[Literal]})${typeSignature[Literal]}"))
+      //        instructions.add(new VarInsnNode(POP,0))
+    }
+    //    case `:-`(l, r) => processExpression(l) - processExpression(r)
+    //    case `/`(l, r) => processExpression(l) / processExpression(r)
+    //    case `*`(l, r) => processExpression(l) * processExpression(r)
+    //    case `%`(l, r) => processExpression(l) % processExpression(r)
+    //    case `!:`(r) => !processExpression(r)
+    //    case `[]`(l,r) => processExpression(l).apply(processExpression(r))
+    //    case `~:`(r) => ~processExpression(r)
+    //    case `-:`(r) => -processExpression(r)
+    //    case `+:`(r) => +processExpression(r)
+    //    case `++:`(v) => val result = processExpression(GerVar(v)) + DecimalLiteral(BigDecimal(1)); processAssignment(`=`(v, LiteralExpression(result))); result
+    //    case `--:`(v) => val result = processExpression(GerVar(v)) + DecimalLiteral(BigDecimal(1)); processAssignment(`=`(v, LiteralExpression(result))); result
+    //    case `:++`(v) => val result = processExpression(GerVar(v)); processAssignment(`=`(v, LiteralExpression(result + DecimalLiteral(BigDecimal(1))))); result
+    //    case `:--`(v) => val result = processExpression(GerVar(v)); processAssignment(`=`(v, LiteralExpression(result - DecimalLiteral(BigDecimal(1))))); result
+    //    case `>>`(l, r) => processExpression(l) >> processExpression(r)
+    //    case `<<`(l, r) => processExpression(l) << processExpression(r)
+    //    case `&`(l, r) => processExpression(l) & processExpression(r)
+    //    case `^`(l, r) => processExpression(l) ^ processExpression(r)
+    //    case `|`(l, r) => processExpression(l) | processExpression(r)
+    //    case `&&`(l, r) => processExpression(l) && processExpression(r)
+    //    case `||`(l, r) => processExpression(l) || processExpression(r)
+    //    case `:==`(l, r) => processExpression(l) == processExpression(r)
+    //    case `:!=`(l, r) => processExpression(l) != processExpression(r)
+    //    case `<`(l, r) => processExpression(l) < processExpression(r)
+    //    case `>`(l, r) => processExpression(l) > processExpression(r)
+    //    case `>=`(l, r) => processExpression(l) >= processExpression(r)
+    //    case `<=`(l, r) => processExpression(l) <= processExpression(r)
+    //    case GerVar(v: LocalVariable) => localContext(v)
+    //    case GerVar(v: GlobalVairable) => globalContext(v)
+    //    case `call`(n,a) => FunctionInvoker.invoke(n,a.map(x => processExpression(mv,x)))
+  }
+
+}
+
+
+object ScriptCompiler {
+
+  def compile(s: Script) = new Generator(s.block, generateClassName(s)).compile
+
   private def generateClassName(s: Script): String = {
     val md = MessageDigest.getInstance("MD5")
     val digest = md.digest(s.toString.getBytes)
@@ -176,8 +192,10 @@ object ScriptCompiler {
 
 
   def main(args: Array[String]) {
-    val s = EvalScriptParser.load("1+1")
+    val s = EvalScriptParser.load("1+1+20+40+90")
     val cs = compile(s)
-    cs.getConstructor(classOf[GlobalContext]).newInstance(new GlobalContext(Map.empty[String, Any])).execute
+    val instance = cs.getConstructor(classOf[GlobalContext]).newInstance(new GlobalContext(Map.empty[String, Any]))
+    instance.execute
   }
 }
+
